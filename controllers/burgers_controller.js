4a5ae4049bce775a685a9ccff5cf7c5becf69513
Shinelin <s.shinelin@gmail.com>
2017-05-06 20:58:21 -0400

    Bug fixes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,36 +6,36 @@ var express = require("express");
 // Routes
 module.exports = function(app) {
     // GET route to redirect
-    app.get('/', function(req, res) {
+    /*app.get('/', function(req, res) {
         res.redirect("/api/all");
-    });
+    });*/
 
     // GET route to show all burgers
-    app.get("/api/all", function(request, response) {
+    app.get("/", function(request, response) {
         db.burgers.findAll({}).then(function(dbResponse) {
             response.render("index", { burgers: dbResponse });
         });
     });
 
     // POST route for saving a new post
-    app.post("/api/burgers/create", function(req, res) {
-        console.log(req.body.burger_name);
+    app.post("/burgers/create", function(request, response) {
+        //console.log("Request: "+request.body);
         db.burgers.create({
-            burger_name: req.body.burger_name
+            burger_name: request.body.burger_name,
+            devoured: request.body.devoured
         }).then(function(dbPost) {
-            res.redirect("/api/all");
+            response.redirect("/");
         });
     });
 
     // PUT route for updating devoured value
-    app.put("/api/:id", function(req, res) {
+    app.put("/burgers/update/:id", function(req, res) {
         db.burgers.update({ devoured: 1 }, {
             where: {
-                id: req.body.id
+                id: req.params.id
             },
         }).then(function(dbPost) {
-            res.redirect("/api/all");
-            console.log("dbpost:" + dbPost);
+            res.redirect("/");
         });
     });
 };
